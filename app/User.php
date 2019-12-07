a8ad22af634951703771bdcba1eb0da855f6cbf2
awais512 <raz.awais25@gmail.com>
2019-12-07 14:14:25 +0500

    Email Verification

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Discussion;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
--- resources/views/users/notifications.blade.php
+++ resources/views/users/notifications.blade.php
@@ -12,13 +12,13 @@
                         @if ($notification->type=== 'App\Notifications\NewReply')
                             A new Reply has been added to your Discussion:
                             <strong>{{$notification->data['discussion']['title']}}</strong>
-                            <a href="{{route('discussions.show', $notification->data['discussion']['title'] )}}" class="btn btn-sm btn-success float-right">View</a>
+                            <a href="{{route('discussions.show', $notification->data['discussion']['slug'] )}}" class="btn btn-sm btn-success float-right">View</a>
                         @endif
 
                         @if ($notification->type=== 'App\Notifications\ReplyMarkedAsBest')
                             Reply has been marked as best: 
                             <strong>{{$notification->data['discussion']['title']}}</strong>
-                            <a href="{{route('discussions.show', $notification->data['discussion']['title'] )}}" class="btn btn-sm btn-success float-right">View</a>
+                            <a href="{{route('discussions.show', $notification->data['discussion']['slug'] )}}" class="btn btn-sm btn-success float-right">View</a>
 
                         @endif
                     </li>
