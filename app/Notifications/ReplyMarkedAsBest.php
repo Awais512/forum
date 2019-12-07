3c60ede1917c7ae2f17b54bdfa7de02a39da6ccf
awais512 <raz.awais25@gmail.com>
2019-12-07 13:20:52 +0500

    Laravel queue work

--- app/Notifications/ReplyMarkedAsBest.php
+++ app/Notifications/ReplyMarkedAsBest.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ReplyMarkedAsBest extends Notification
+class ReplyMarkedAsBest extends Notification implements ShouldQueue
 {
     use Queueable;
     public $discussion;
